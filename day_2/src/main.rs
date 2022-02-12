6ac77ba760c96d7d298979c83b2c808b68977598
salpreh <salva.perez46@gmail.com>
2022-02-12 23:54:57 +0100

    Updated kata crates to use common lib functions

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,15 +1,15 @@
-mod submarine;
+use std::io::{Error, ErrorKind};
 
-use std::fs::File;
-use std::io::{BufReader, prelude::*, Error, ErrorKind};
-use std::env;
+use common::{FileConfig, get_input_file_path, load_data};
+
+mod submarine;
 
 const INPUT_DATA_FILE: &str = "./day_2/resources/input_data.txt";
 const TEST_INPUT_DATA_FILE: &str = ".day_2/resources/min_input_data.txt";
 
 fn main() {
-    let env_arg: Option<String> = env::args().nth(1);
-    let commands = load_commands_data(get_input_file_path(env_arg));
+    let config = FileConfig::new(INPUT_DATA_FILE, TEST_INPUT_DATA_FILE);
+    let commands = load_commands_data(get_input_file_path(config));
 
     let mut submarine = submarine::AdvancedSubmarine::new();
     for command in commands {
@@ -43,26 +43,4 @@ fn load_commands_data(path: &str) -> Vec<submarine::Command> {
     }
 
     commands
-}
-
-fn load_data(path: &str) -> Vec<String> {
-    let file = File::open(path)
-        .expect("Error loading file");
-    let reader = BufReader::new(file);
-
-    let mut data: Vec<String> = Vec::new();
-    for line in reader.lines() {
-        if let Ok(d) = line {
-            data.push(String::from(d.trim()));
-        };
-    }
-
-    data
-}
-
-fn get_input_file_path(env: Option<String>) -> &'static str {
-    match env {
-        Some(s) => if s == "test" {TEST_INPUT_DATA_FILE} else {INPUT_DATA_FILE},
-        _ => INPUT_DATA_FILE
-    }
 }
--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,8 +1,6 @@
 mod diagnostic_parse;
 
-use std::fs::File;
-use std::io::{BufReader, prelude::*};
-use std::env;
+use common::{FileConfig, get_input_file_path, load_data};
 
 use diagnostic_parse::{DiagnosticReport, parse_report_line};
 
@@ -10,8 +8,8 @@ const INPUT_DATA_FILE: &str = "./day_3/resources/input_data.txt";
 const TEST_INPUT_DATA_FILE: &str = "./day_3/resources/min_input_data.txt";
 
 fn main() {
-    let env_arg: Option<String> = env::args().nth(1);
-    let report_data = load_data(get_input_file_path(env_arg));
+    let config = FileConfig::new(INPUT_DATA_FILE, TEST_INPUT_DATA_FILE);
+    let report_data = load_data(get_input_file_path(config));
 
     let diagnostic_report = DiagnosticReport::from_data(report_data);
 
@@ -35,26 +33,4 @@ fn load_report_data(path: &str) -> Vec<u32> {
     load_data(path).into_iter()
         .map(|i| parse_report_line(&i))
         .collect()
-}
-
-fn load_data(path: &str) -> Vec<String> {
-    let file = File::open(path)
-        .expect("Error loading file");
-    let reader = BufReader::new(file);
-
-    let mut data: Vec<String> = Vec::new();
-    for line in reader.lines() {
-        if let Ok(d) = line {
-            data.push(String::from(d.trim()));
-        };
-    }
-
-    data
-}
-
-fn get_input_file_path(env: Option<String>) -> &'static str {
-    match env {
-        Some(s) => if s == "test" {TEST_INPUT_DATA_FILE} else {INPUT_DATA_FILE},
-        _ => INPUT_DATA_FILE
-    }
 }
